c57fee35dd9b97affaebc5e048f3c3db18ae4395
Alex Barkell <albarkel@umich.edu>
2022-07-13 16:18:19 -0400

    remove unused dependencies

--- src/main/java/org/kpmp/Query.java
+++ src/main/java/org/kpmp/Query.java
@@ -5,8 +5,6 @@ import org.kpmp.spatialViewerDataset.SpatialViewerDataset;
 import org.kpmp.spatialViewerDataset.SpatialViewerDatasetRepository;
 import org.kpmp.spatialViewerDataset.SpatialViewerExternalLink;
 import org.kpmp.spatialViewerDataset.SpatialViewerExternalLinkRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +16,6 @@ public class Query implements GraphQLQueryResolver {
 
 	private SpatialViewerDatasetRepository spatialViewerDatasetRepository;
 	private SpatialViewerExternalLinkRepository spatialViewerExternalLinkRepository;
-	private Logger logger = LoggerFactory.getLogger(Query.class);
 
 	@Autowired
 	public Query(
