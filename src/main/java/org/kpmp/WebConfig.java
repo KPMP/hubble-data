58a5269becb93d35761c0145885b0f94a4bcb6f3
Zach Wright <zwright@umich.edu>
2023-06-12 11:45:36 -0400

    KPMP-4285: Double CORS config

--- src/main/java/org/kpmp/WebConfig.java
+++ src/main/java/org/kpmp/WebConfig.java
@@ -4,22 +4,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 @EnableAutoConfiguration
 public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		CorsRegistration corsRegistration = registry.addMapping("/**");
-		corsRegistration.allowedOrigins("*");
-		corsRegistration.allowedMethods("GET", "POST");
-	}
-
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
