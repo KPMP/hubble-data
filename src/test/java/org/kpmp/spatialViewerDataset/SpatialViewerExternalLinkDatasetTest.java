1f11a2079307aeddfdbf3fbbc856a83689a129c3
Dert1129 <nathancreger1111@gmail.com>
2023-08-31 13:49:08 -0400

    Add tests

--- src/test/java/org/kpmp/spatialViewerDataset/SpatialViewerExternalLinkDatasetTest.java
+++ src/test/java/org/kpmp/spatialViewerDataset/SpatialViewerExternalLinkDatasetTest.java
@@ -98,4 +98,16 @@ public class SpatialViewerExternalLinkDatasetTest {
         spatialViewerExternalLink.setTissueType("12345");
         assertEquals("12345", spatialViewerExternalLink.getTissueType());
     }
+
+    @Test
+    public void setReleaseVersionDisplay(){
+        spatialViewerExternalLink.setReleaseVersionDisplay("Recently Released");
+        assertEquals("Recently Released", spatialViewerExternalLink.getReleaseVersionDisplay());
+    }
+
+    @Test
+    public void setReleaseVersion(){
+        spatialViewerExternalLink.setReleaseVersion(45.0);
+        assertEquals(45.0, spatialViewerExternalLink.getReleaseVersionDisplay());
+    }
 }
